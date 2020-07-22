78d2fbeb7291c5dc35eb7ce0e6438df8e802eca2
Andrew Andersen <andersac88@gmail.com>
2020-07-21 20:26:56 -0500

    updates

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,22 +2,23 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
-	const [term, setTerm] = useState("programming");
+	const [term, setTerm] = useState("cat");
     const [results, setResults] = useState([]);
-
 
 	useEffect(() => {
 		const search = async () => {
-            const { data } = await axios.get("https://en.wikipedia.org/w/api/php", {
+            const { data } = await axios.get(`https://en.wikipedia.org/w/api/php`, {
 				params: {
 					action: "query",
-                    list: "search",
+					origin: '*',
                     format: "json",
-					origin: "*",
 					srsearch: term,
+					list: "search",
+					
 				},
 			});
 			setResults(data);
+			console.log(data)
 		};
 
 		search();
