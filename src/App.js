8284ebad60caa3ba58b87486c4c3f5730fd3b92d
Andrew Andersen <andersac88@gmail.com>
2020-07-26 13:03:49 -0500

    hooks useRef

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
-
+import Dropdown from "./components/Dropdown";
 
 const items = [
-    {
-        title: "What is React",
-        content: "React is a front end Javascript frame-work"
-    },
-    {
-        title: "Why use React",
-        content: "React is a favorite JS library among engineers"
-    },
-    {
-        title: "How do you use React",
-        content: "You use React by creating components"
-    }
-]
+	{
+		title: "What is React",
+		content: "React is a front end Javascript frame-work",
+	},
+	{
+		title: "Why use React",
+		content: "React is a favorite JS library among engineers",
+	},
+	{
+		title: "How do you use React",
+		content: "You use React by creating components",
+	},
+];
+
+const options = [
+	{
+		label: "The Color Red",
+		value: "red",
+	},
+	{
+		label: "The Color Green",
+		value: "green",
+	},
+	{
+		label: "The Shade of Blue",
+		value: "blue",
+	},
+];
 
 export default () => {
+	const [selected, setSelected] = useState(options[0]);
+	const [showDropdown, setShowDropdown] = useState(true);
+
 	return (
 		<>
-			<Search />
+			<button onClick={() => setShowDropdown(!showDropdown)}>
+				Toggle Dropdown
+			</button>
+			{showDropdown ? (
+				<Dropdown
+					selected={selected}
+					onSelectedChange={setSelected}
+					options={options}
+				/>
+			) : null}
 		</>
 	);
 };
